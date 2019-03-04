31270b08c57dfcdb563d52f69249f5fb4d4c215f
Daniel Sef <danielsef@icloud.com>
2019-03-04 07:40:03 +0100

    Update wfrun.sh

--- wfrun.sh
+++ wfrun.sh
@@ -27,3 +27,32 @@ python /Users/danielsef/repos/recomm_pipeline/workflowrunner.py
 # 24.) Streamsets: Reset the Pipeline(Crean state cache); CAll File => HBASE Loader Pipeline   Result: AggrFeaturedDb.json is loaded to HBASE (The Pipeline will be in finished state when all rows will be loaded)
 
 
+
+
+
+
+-----------------------------------------------------------------------------------------------
+
+
+#!/bin/bash
+
+echo "Positional Parameters"
+echo '$0 = ' $0
+echo '$1 = ' $1
+echo '$2 = ' $2
+echo '$3 = ' $3
+
+
+HOST="$HOSTNAME"
+RIGHT_NOW=$(date +"%Y%m%d")
+TIME_STAMP="Updated on $RIGHT_NOW by $USER"
+
+echo $HOST
+echo $RIGHT_NOW
+echo $TIME_STAMP
+
+FILES=/Users/danielsef/repos/recomm-feature-flask-v03/*
+for f in $FILES
+do
+  echo "Processing $f file..."
+done
